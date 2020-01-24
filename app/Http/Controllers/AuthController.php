0470b13f1a1718e6069a3439ccc510e024590faf
kajal odedara <kajal.o@crestinfosystems.net>
2020-01-24 17:34:37 +0530

    create passport auth with generate token

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\PassportServiceProvider;
+
+class AuthController extends Controller
+{
+    public $successStatus = 200;
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:3',
+            'c_password' => 'required|same:password',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('myapp')->accessToken;
+        return response()->json(['success' => $success], $this->successStatus);
+    }
+
+
+    public function login()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('myapp')->accessToken;
+            return response()->json(['success' => $success], $this->successStatus);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    public function getUser()
+    {
+        $user = Auth::user();
+        // dd($user);
+        return response()->json(['success' => $user], $this->successStatus);
+        return response()->json(['get user successfully']);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,12 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
+
+  Route::post('login', 'AuthController@login');
+  Route::post('register', 'AuthController@register');
+//   Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('getUser', 'AuthController@getUser');
+//   });
+
